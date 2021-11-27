b90216b934a48c56bfd73a9c00732c925efb79a1
FlorianDrouet46 <florian.drouet78@gmail.com>
2021-11-27 17:29:21 +0100

    feat: rewrite main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import email
 from func.giver_receiver import get_giver_receiver
 from func.gmail_func import get_mails, send_mails
 
-names = ["amelie", "victoria", "benjamin", "florian", "carolina", "matthieu"]
-constraints = set([("florian", "victoria"), ("benjamin", "amelie"), ("matthieu", "carolina")])
+names, emails, wish_list = get_mails("Secret Santa 2021")
 
-dict_giver_receiver = get_giver_receiver(names, constraints)
+#constraints = set([("florian", "victoria"), ("benjamin", "amelie"), ("matthieu", "carolina")])
+
+dict_giver_receiver = get_giver_receiver(names=names, constraints=None)
 print(dict_giver_receiver)
-
-names, emails, wish_list, = get_mails("Secret Santa 2021")
 
 print(names)
 print(emails)
 
-#send_mails(dict_giver_receiver=dict_giver_receiver, emails=emails)
+send_mails(dict_giver_receiver=dict_giver_receiver, emails=emails, wish_list=wish_list)
