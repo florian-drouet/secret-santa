a3acdb37d58c3df9129bd11b0d38fd3add69fdf8
FlorianDrouet46 <florian.drouet78@gmail.com>
2021-11-27 17:33:38 +0100

    fix: modify main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from func.gmail_func import get_mails, send_mails
 
 names, emails, wish_list = get_mails("Secret Santa 2021")
 
-#constraints = set([("florian", "victoria"), ("benjamin", "amelie"), ("matthieu", "carolina")])
-
-dict_giver_receiver = get_giver_receiver(names=names, constraints=None)
-print(dict_giver_receiver)
-
 print(names)
 print(emails)
 
-send_mails(dict_giver_receiver=dict_giver_receiver, emails=emails, wish_list=wish_list)
+#constraints = set([("florian", "victoria"), ("benjamin", "amelie"), ("matthieu", "carolina")])
+dict_giver_receiver = get_giver_receiver(names=names, constraints=None)
+
+#send_mails(dict_giver_receiver=dict_giver_receiver, emails=emails, wish_list=wish_list)
