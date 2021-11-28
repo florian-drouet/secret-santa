37b87857e0a9bb2f7aa0c00744d35d7fef0cce5b
FlorianDrouet46 <florian.drouet78@gmail.com>
2021-11-28 13:24:51 +0100

    feat: refacto giver_receiver_script

--- func/giver_receiver.py
+++ func/giver_receiver.py
@@ -1,17 +1,16 @@
+from os import name
 import sys
 import random
 import numpy as np
 import pandas as pd
 
-
-def get_giver_receiver(names, constraints=None):
+def init_matrix(names, constraints=None):
+    """Function used to initialize matrix of participants with constraints"""
     number_of_participants = len(names)
 
-    if number_of_participants <= 1:
+    if number_of_participants <= 2:
         print('WARNING: not enough participants')
         sys.exit(1)
-
-    random_seed = random.randrange(1000)
 
     matrix = np.ones((len(names), len(names)))
     np.fill_diagonal(matrix, 0)
@@ -21,18 +20,20 @@ def get_giver_receiver(names, constraints=None):
         for constraint in constraints:
             guests.loc[constraint[0]][constraint[1]] = 0
             guests.loc[constraint[1]][constraint[0]] = 0
+    
+    return guests
+
+
+def get_giver_receiver(names, constraints):
+    """Function used to define who gifts who"""
+
+    guests = init_matrix(names=names, constraints=constraints)
+    random_seed = random.randrange(1000)
 
     while guests.sum().sum() != 0:
         dict_giver_receiver = dict()
-        matrix = np.ones((len(names), len(names)))
-        np.fill_diagonal(matrix, 0)
-        guests = pd.DataFrame(matrix, index=names, columns=names).astype(int)
-
-        if constraints is not None:
-            for constraint in constraints:
-                guests.loc[constraint[0]][constraint[1]] = 0
-                guests.loc[constraint[1]][constraint[0]] = 0
-        
+        guests = init_matrix(names=names, constraints=constraints)
+        print(guests)
         random_givers = random.sample(list(guests.index), k=len(names))
             
         for random_giver in random_givers:
@@ -41,8 +42,7 @@ def get_giver_receiver(names, constraints=None):
             except:
                 random_seed += 1
             guests[receiver] = 0
-            if number_of_participants > 2:
-                guests.loc[receiver, random_giver] = 0
+            guests.loc[receiver, random_giver] = 0
             dict_giver_receiver[random_giver] = receiver
     
     return dict_giver_receiver
